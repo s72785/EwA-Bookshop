0c84ce63c3412743acc835baed6e28225ef4a2da
s72785 <s72785@mobile.workstation.net>
2015-11-18 17:07:12 +0100

    moving config to separate file, fixing string delimis, emphasizing SQL key words

--- shop/uebersicht.php
+++ shop/uebersicht.php
@@ -1,24 +1,28 @@
 <?php
-mysql_connect("localhost", "G05", "xxx") or
-    die("Keine Verbindung möglich: " . mysql_error());
-mysql_select_db("g05");
 
-$result = mysql_query("SELECT bu.id as id, barcode, titel, au.name as autor FROM buecher as bu join autor as au On au.id=bu.autorid");
+/* access data in config file (data not inside repo) */
+include_once( 'dbconf.php' );
 
-if($result === FALSE) { 
-    die(mysql_error()); // TODO: better error handling
+mysql_connect( $dbhost, $dbuser, $dbpass ) or
+	die( 'Keine Verbindung möglich: ' . mysql_error() );
+mysql_select_db( $dbname );
+
+$result = mysql_query( 'SELECT bu.id AS id, barcode, titel, au.name AS autor FROM buecher AS bu JOIN autor AS au ON au.id = bu.autorid' );
+
+if( $result === FALSE ) { 
+	die( mysql_error() ); // TODO: better error handling
 }
 
-echo "<table border=\"1\">";
-while ($row = mysql_fetch_array($result)) {
-    echo "<tr>";
-    echo "<td>".$row['barcode']."</td>";
-    echo "<td><h2><a href=\"details.php?id=".$row['id']."\">".$row['titel']."</a></h2></td>";
-    echo "<td>".$row['autor']."</td>";
-    echo "</tr>";
+echo '<table border="1">';
+while ( $row = mysql_fetch_array( $result ) ) {
+	echo '<tr>';
+	echo '<td>' . $row['barcode'] . '</td>';
+	echo '<td><h2><a href="details.php?id=' . $row['id'] . '">' . $row['titel'] . '</a></h2></td>';
+	echo '<td>' . $row['autor'] . '</td>';
+	echo '</tr>';
 }
-echo "</table>";  
+echo '</table>';  
 
+mysql_free_result( $result );
 
-mysql_free_result($result);
 ?>
