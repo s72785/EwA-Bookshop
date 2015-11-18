647751fbfcb8b2938f366f8e9f6361daf79f4ebd
s72785 <s72785@mobile.workstation.net>
2015-11-18 17:36:39 +0100

    fixes on details.php

---
+++ shop/details.php
@@ -0,0 +1,34 @@
+<?php
+
+echo( '<!doctype html><html><head><title>Bookshop - Übersicht</title><meta charset="UTF-8"></head><body>' );
+
+/* access data in config file (data not inside repo) */
+include_once( 'dbconf.php' );
+
+$dblink = mysql_connect( $dbhost, $dbuser, $dbpass ) or
+	die( 'Keine Verbindung möglich: ' . mysql_error() );
+mysql_select_db( $dbname );
+
+$result = mysql_query('SELECT bu.id, barcode, titel, au.name AS autor FROM buecher AS bu JOIN autor AS au ON au.id = bu.autorid WHERE bu.id='."'" . (INT) $_GET['id'] . "'");
+
+if($result === FALSE) { 
+	die( mysql_error() ); // TODO: better error handling
+}
+
+echo( '<table border="1">' );
+while ( $row = mysql_fetch_array( $result ) ) {
+	echo( '<tr>'
+	. '<td>' . utf8_encode( $row['barcode'] ) . '</td>'
+	. '<td><h2>' . utf8_encode( $row['titel'] ) . '</h2></td>'
+	. '<td>' . utf8_encode( $row['autor'] ) . '</td>'
+	. '</tr>' );
+}
+echo( '</table>' );  
+
+mysql_free_result($result);
+
+mysql_close( $dblink );
+
+echo('</body></html>');
+
+?>
