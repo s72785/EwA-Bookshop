f3d9bde48b81981ea11f68589b648f5468160a85
s72785 <s72785@mobile.workstation.net>
2015-11-18 17:14:12 +0100

    encode strings to proper utf8, use fucntion syntax, close handles that are opened

--- shop/uebersicht.php
+++ shop/uebersicht.php
@@ -1,28 +1,34 @@
 <?php
 
+echo( '<!doctype html><html><head><title>Bookshop - Übersicht</title><meta charset="UTF-8"></head><body>' );
+
 /* access data in config file (data not inside repo) */
 include_once( 'dbconf.php' );
 
-mysql_connect( $dbhost, $dbuser, $dbpass ) or
+$dblink = mysql_connect( $dbhost, $dbuser, $dbpass ) or
 	die( 'Keine Verbindung möglich: ' . mysql_error() );
 mysql_select_db( $dbname );
 
 $result = mysql_query( 'SELECT bu.id AS id, barcode, titel, au.name AS autor FROM buecher AS bu JOIN autor AS au ON au.id = bu.autorid' );
 
-if( $result === FALSE ) { 
+if( $result === FALSE ) {
 	die( mysql_error() ); // TODO: better error handling
 }
 
-echo '<table border="1">';
+echo( '<table border="1">' );
 while ( $row = mysql_fetch_array( $result ) ) {
-	echo '<tr>';
-	echo '<td>' . $row['barcode'] . '</td>';
-	echo '<td><h2><a href="details.php?id=' . $row['id'] . '">' . $row['titel'] . '</a></h2></td>';
-	echo '<td>' . $row['autor'] . '</td>';
-	echo '</tr>';
+	echo( '<tr>'
+	. '<td>' . utf8_encode( $row['barcode'] ) . '</td>'
+	. '<td><h2><a href="details.php?id=' . utf8_encode( $row['id'] ) . '">' . utf8_encode( $row['titel'] ) . '</a></h2></td>'
+	. '<td>' . utf8_encode( $row['autor'] ) . '</td>'
+	. '</tr>' );
 }
-echo '</table>';  
+echo( '</table>' );
 
 mysql_free_result( $result );
+
+mysql_close( $dblink );
+
+echo('</body></html>');
 
 ?>
