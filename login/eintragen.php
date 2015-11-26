d77d71da3b04169936cc66146005336114e3e6ec
s72785 <s72785@mobile.workstation.net>
2015-11-26 14:30:46 +0100

    using hasing functions as in login

--- login/eintragen.php
+++ login/eintragen.php
@@ -1,45 +1,41 @@
+<!doctype html><html><head><title>Bookshop - Login</title><meta charset="UTF-8">
 <?php
 	/* access data in config file (data not inside repo) */
 	include_once( '' /**/ . '../shop/' /**/ . 'dbconf.php' );
-	
+	include_once( '' /**/ . '../login/' /**/ . 'functions.php' );
+
 	$verbindung = mysql_connect($dbhost, $dbuser, $dbpass)
 	or die("Verbindung zur Datenbank konnte nicht hergestellt werden");
 
 	mysql_select_db($dbname) or die ("Datenbank konnte nicht ausgewählt werden");
 
-	$username = $_POST["username"];
-	$password = $_POST["password"];
-	$password2 = $_POST["password2"];
-	$name = $_POST["name"];
-	$address = $_POST["address"];
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$password2 = $_POST['password2'];
+	$name = $_POST['name'];
+	$address = $_POST['address'];
 
-	if($password != $password2 OR $username == "" OR $password == "" OR $name == "" OR $address == "")
-	{
-		echo "Eingabefehler. Bitte alle Felder korekt ausfüllen. <a href=\"eintragen.html\">Zurück</a>";
+	if($password != $password2 OR $username == "" OR $password == "" OR $name == "" OR $address == "") {
+		echo)( 'Eingabefehler. Bitte alle Felder korekt ausfüllen. <a href="eintragen.html">Zurück</a>' );
 		exit;
 	}
-	$password = md5($password);
+	$password = pwd_hash_old( $password );
 
-	$result = mysql_query("SELECT UserID FROM User WHERE Username LIKE '$username'");
+	$result = mysql_query("SELECT UserID FROM User WHERE Username = '".$username."';");
 	$menge = mysql_num_rows($result);
 
-	if($menge == 0)
-	{
-		$eintrag = "INSERT INTO User (Username, Userpwmd5, UserAnrede, UserAdresse) VALUES ('$username', '$password', '$name', '$address')";
-		$eintragen = mysql_query($eintrag);
+	if( $menge == 0 ) {
+		$eintrag = "INSERT INTO User (Username, Userpwmd5, UserAnrede, UserAdresse) VALUES ('".$username."', '".$password."', '".$name."', '".$address."');";
+		$eintragen = mysql_query( $eintrag );
 
-		if($eintragen == true)
-		{
-			echo "Benutzername <b>$username</b> wurde erstellt. <a href=\"login.html\">Login</a>";
+		if($eintragen == true) {
+			echo( 'Benutzername <b>'.$username.'</b> wurde erstellt. <a href="login.html">Login</a>' );
+		} else {
+			echo( 'Fehler beim Speichern des Benutzernames. <a href="eintragen.html">Zurück</a>' );
 		}
-		else
-		{
-			echo "Fehler beim Speichern des Benutzernames. <a href=\"eintragen.html\">Zurück</a>";
-		}
+	} else {
+		echo( 'Benutzername schon vorhanden. <a href="eintragen.html">Zurück</a>' );
 	}
 
-	else
-	{
-		echo "Benutzername schon vorhanden. <a href=\"eintragen.html\">Zurück</a>";
-	}
-?> 
+	mysql_close( $verbindung );
+?>
