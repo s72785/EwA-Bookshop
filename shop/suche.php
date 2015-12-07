18b4ff192a6be88a6c18c7d147c458cbae4a6e45
patu radFeld <opatu@posteo.de>
2015-12-07 14:35:51 +0100

    suche nun per ajax abfrage

--- shop/suche.php
+++ shop/suche.php
@@ -1,54 +1,33 @@
 <?php
-echo( '<!doctype html><html><head><title>Bookshop - Übersicht</title><meta charset="UTF-8"></head><body>' );
-
-/* access data in config file (data not inside repo) */
-include_once( 'dbconf.php' );
-
-
+echo( '<!doctype html><html><head><title>Bookshop - Übersicht</title><meta charset="UTF-8"><script src="../js/jquery-2.1.4.min.js"></script></head><body>' );
 ?>
  <form action="suche.php" method="post">
 	  <label for="suche">Suche</label><br>
 	  <input type="text" name="suche" id="suche">
 	  <br>
-	  <input type="submit" value="Submit">	 
+	  <input type="submit" value="Submit" id="submit">	 
 </form> 
+<div id="main">
 <?php
-
-$dblink = mysql_connect( $dbhost, $dbuser, $dbpass ) or
-	die( 'Keine Verbindung möglich: ' . mysql_error() );
-mysql_select_db( $dbname );
-
-$suche = (isset($_POST['suche'])) ? $_POST['suche'] : '' ;
-
-$sql = 'SELECT bu.id AS id, barcode, titel, au.name AS autor FROM 
-						buecher AS bu JOIN autor AS au ON au.id = bu.autorid
-						JOIN verlag AS ve ON ve.id = bu.verlagsid
-						where (
-							titel like \'%' . mysql_escape_string( $suche ) . '%\' or 
-							au.name like \'%' . mysql_escape_string( $suche ) . '%\' or
-							ve.name like \'%' . mysql_escape_string( $suche ) . '%\'
-						)';
-//echo $sql;
-$result = mysql_query( $sql );
-
-if( $result === FALSE ) {
-	die( mysql_error() ); // TODO: better error handling
-}
-
-echo( '<table border="1">' );
-while ( $row = mysql_fetch_array( $result ) ) {
-	echo( '<tr>'
-	. '<td>' . utf8_encode( $row['barcode'] ) . '</td>'
-	. '<td><h2><a href="details.php?id=' . utf8_encode( $row['id'] ) . '">' . utf8_encode( $row['titel'] ) . '</a></h2></td>'
-	. '<td>' . utf8_encode( $row['autor'] ) . '</td>'
-	. '</tr>' );
-}
-echo( '</table>' );
-
-mysql_free_result( $result );
-
-mysql_close( $dblink );
-
-
-echo('</body></html>');
+include_once("ergebnisse.php");
 ?>
+</div>
+<script type="text/javascript">
+	
+	function suche(suche)
+	{
+		$.ajax({
+		  url: "ergebnisse.php",
+		  method: "POST",
+		  data: { suche: suche }
+		})
+		  .done(function( html ) {
+				$( "#main" ).html( html );
+		  });
+	} 
+	$('#suche').keyup(function(){		
+		suche($('#suche').val());
+	});
+
+ </script>
+</body></html>
