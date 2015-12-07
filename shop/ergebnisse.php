64b888905c2ed590deea4a26eb5f950184685e8f
patu radFeld <opatu@posteo.de>
2015-12-07 14:36:25 +0100

    Ergebnis Ausgabe

---
+++ shop/ergebnisse.php
@@ -0,0 +1,35 @@
+<?php
+/* access data in config file (data not inside repo) */
+include_once( 'dbconf.php' );
+
+$dblink = mysql_connect( $dbhost, $dbuser, $dbpass ) or
+	die( 'Keine Verbindung möglich: ' . mysql_error() );
+mysql_select_db( $dbname );
+$suche = (isset($_POST['suche'])) ? $_POST['suche'] : '' ;
+
+$sql = 'SELECT bu.id AS id, barcode, titel, au.name AS autor FROM 
+						buecher AS bu JOIN autor AS au ON au.id = bu.autorid
+						JOIN verlag AS ve ON ve.id = bu.verlagsid
+						where (
+							titel like \'%' . mysql_escape_string( $suche ) . '%\' or 
+							au.name like \'%' . mysql_escape_string( $suche ) . '%\' or
+							ve.name like \'%' . mysql_escape_string( $suche ) . '%\'
+						)';
+//echo $sql;
+$result = mysql_query( $sql );
+if( $result === FALSE ) {
+	die( mysql_error() ); // TODO: better error handling
+}
+echo( '<table border="1">' );
+while ( $row = mysql_fetch_array( $result ) ) {
+	echo( '<tr>'
+	. '<td>' . utf8_encode( $row['barcode'] ) . '</td>'
+	. '<td><h2><a href="details.php?id=' . utf8_encode( $row['id'] ) . '">' . utf8_encode( $row['titel'] ) . '</a></h2></td>'
+	. '<td>' . utf8_encode( $row['autor'] ) . '</td>'
+	. '</tr>' );
+}
+echo( '</table>' );
+mysql_free_result( $result );
+mysql_close( $dblink );
+
+?>
