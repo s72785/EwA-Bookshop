be4eed55a8803c1452f48578a86cee02231cc4b4
s72785 <s72785@mobile.workstation.net>
2016-01-20 14:39:43 +0100

    zufallstest in php

---
+++ shop/random.php
@@ -0,0 +1,24 @@
+<?php
+// Create SoapClient with wsdl address
+$client = new SoapClient("http://141.56.131.108:8080/Reseller/SampleService/?wsdl");
+// Create parameter array
+$param_arr = array ( 'maxValue' => 123 ); 
+// Call operation with parameter
+$response=$client->GetRandom($param_arr);
+// Convert response to useable array
+$response_arr=objectToArray($response);
+// Extract array from response structure
+$getRandomResult=$response_arr['GetRandomResult'];
+echo"GetRandom(...) returns: ".$getRandomResult;
+// Helper functionto convert an object to an array
+function objectToArray($d){
+	if(is_object($d)){
+		$d=get_object_vars($d);
+	}
+	if(is_array($d)){
+		return array_map(__FUNCTION__,$d);
+		}else{
+			return $d;
+		}
+	}
+?>
