6d163a031a687fe0f500f2303c150fc52a068c4f
s72785 <s72785@mobile.workstation.net>
2016-01-24 02:58:05 +0100

    about eingebaut, mehr detail auf gruppenseite

---
+++ about/g05.php
@@ -0,0 +1,28 @@
+<h3>Kleinigkeiten über die Gruppe</h3>
+
+<table style="width: 100%;">
+	<thead>
+	<tr>
+		<th>Am Tisch</th>
+		<th>Person</th>
+		<th>Mehr</th>
+	</tr>
+	</thead>
+	<tbody>
+	<tr id="v" class="one">
+		<td>links</td>
+		<td>vladimir</td>
+		<td><a href="https://github.com/VladVe" target="_blank"><span class="gh-ico" aria-hidden="true" title="github-symbol"></span></a></td>
+	</tr>
+	<tr id="w" class="two">
+		<td>mitte</td>
+		<td>wolf</td>
+		<td><a href="https://github.com/s72785/notes/tree/master/i465/" target="_blank"><span class="gh-ico" aria-hidden="true" title="github-symbol"></span></a>, <a href="https://pgp.mit.edu/pks/lookup?op=get&search=0x9CD3A46248B660CA" title="PGP Public Key">PGP</a></td><!-- mit font-awesome die mieze für github hier einzubinden -->
+	</tr>
+	<tr id="p" class="one">
+		<td>rechts</td>
+		<td>patu</td>
+		<td><a href="https://github.com/patudd" target="_blank"><span class="gh-ico" aria-hidden="true" title="github-symbol"></span></a>, <a href="https://pgp.mit.edu/pks/lookup?op=get&search=0x4959B79A70B1C09D" title="PGP Public Key">PGP</a></td>
+	</tr>
+	</tbody>
+</table>
