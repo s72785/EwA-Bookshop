1fe562c41000ee15fbfabf19487a80373ed5f76e
s72785 <s72785@mobile.workstation.net>
2016-01-25 21:43:51 +0100

    adding links to group info

--- about/g05.php
+++ about/g05.php
@@ -11,17 +11,17 @@
 	<tbody>
 	<tr id="v" class="one">
 		<td>links</td>
-		<td>vladimir</td>
+		<td><a href="www2.htw-dresden.de/~s68465/">vladimir</a></td>
 		<td><a href="https://github.com/VladVe" target="_blank"><span class="gh-ico" aria-hidden="true" title="github-symbol"></span></a></td>
 	</tr>
 	<tr id="w" class="two">
 		<td>mitte</td>
-		<td>wolf</td>
+		<td><a href="http://www2.htw-dresden.de/~s72785/">wolf</a></td>
 		<td><a href="https://github.com/s72785/notes/tree/master/i465/" target="_blank"><span class="gh-ico" aria-hidden="true" title="github-symbol"></span></a>, <a href="https://pgp.mit.edu/pks/lookup?op=get&search=0x9CD3A46248B660CA" title="PGP Public Key">PGP</a></td><!-- mit font-awesome die mieze für github hier einzubinden -->
 	</tr>
 	<tr id="p" class="one">
 		<td>rechts</td>
-		<td>patu</td>
+		<td><a href="www2.htw-dresden.de/~s67191/">patu</a></td>
 		<td><a href="https://github.com/patudd" target="_blank"><span class="gh-ico" aria-hidden="true" title="github-symbol"></span></a>, <a href="https://pgp.mit.edu/pks/lookup?op=get&search=0x4959B79A70B1C09D" title="PGP Public Key">PGP</a></td>
 	</tr>
 	</tbody>
