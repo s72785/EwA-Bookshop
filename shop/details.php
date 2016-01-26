7b774c1a1e44e5879c85390863afcb0f0c754dfb
s72785 <s72785@mobile.workstation.net>
2016-01-26 17:56:21 +0100

    fix: details: mehr details

--- shop/details.php
+++ shop/details.php
@@ -11,18 +11,25 @@ mysql_select_db( $dbname );
 
 if( isset($_GET['id'] ) ) {
 
-$result = mysql_query('SELECT bu.id, barcode, titel, au.name AS autor FROM buecher AS bu JOIN autor AS au ON au.id = bu.autorid'.( ( !isset($_GET['id'] ) )?'':" WHERE bu.id='" . intval($_GET['id']) ) . "'");
+$result = mysql_query(
+'SELECT bu.id, barcode, titel, au.name AS autor, v.name AS verlag, netto AS preis, gewicht FROM buecher AS bu
+	JOIN autor AS au ON au.id = bu.autorid'.( ( !isset($_GET['id'] ) )?'':"
+	JOIN verlag AS v ON v.id = bu.verlagsid
+	WHERE bu.id='" . intval($_GET['id']) ) . "'");
 
 if($result === FALSE) { 
 	die( mysql_error() ); // TODO: better error handling
 }
 
-echo( '<table border="1">' );
+echo( '<table border="1"><tr><th>ISBN</th><th>Titel</th><th>Autor</th><th>Verlag</th><th>Preis</th><th>Gewicht</th></tr>' );
 while ( $row = mysql_fetch_array( $result ) ) {
 	echo( '<tr>'
 	. '<td>' . utf8_encode( $row['barcode'] ) . '</td>'
-	. '<td><h2>' . utf8_encode( $row['titel'] ) . '</h2></td>'
+	.'<td><h2>' . utf8_encode( $row['titel'] ) . '</h2></td>'
 	. '<td>' . utf8_encode( $row['autor'] ) . '</td>'
+	. '<td>' . utf8_encode( $row['verlag'] ) . '</td>'
+	. '<td>' . utf8_encode( number_format($row['preis'], 2, ',', '.') ) . '&nbsp;€</td>'
+	. '<td>' . utf8_encode( number_format($row['gewicht'], 0, ',', '.') ) . '&nbsp;g</td>'
 	. '</tr>' );
 }
 echo( '</table><small><a href="">zurück</a></small>' );  
