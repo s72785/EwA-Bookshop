60dda7f7d0643a24ae5efb917a26156f2f84559f
s72785 <s72785@mobile.workstation.net>
2016-01-27 03:11:37 +0100

    fix: warenkorb leer nach bestellen

--- shop/DoOrder.php
+++ shop/DoOrder.php
@@ -16,14 +16,14 @@ $refresh=15;
 	echo('<h1>Vielen Dank - Seite <a href="/ewa/g05/?show=success">lädt nach '.$refresh.' Sekunden neu</a>.</h1>');
 	//////////////////////////////////////////////////////////////
 	// Author: Michael Heydeck, 04.12.2013
-	
+
 	echo "<h1>Web Service Client: BookTrade - DoOrder</h1>";
 
 	//////////////////////////////////////////////////////////////
 	// WSDL and Web-Service addresses
 	$wsdlAddr = 'http://141.56.131.108:8080/Reseller/BookTrade/?wsdl';
 	$wsAddr = 'http://141.56.131.108:8080/Reseller/BookTrade/';
-	
+
 	echo "Creating SOAP-Client for ".$wsAddr." with wsdl: ".$wsdlAddr."<br/><br/>";
 	//////////////////////////////////////////////////////////////
 	// Web Service Client
@@ -35,7 +35,7 @@ $refresh=15;
 	$OrderPosition = array();
 	$summe=0;
 	if ( !empty( $_SESSION["artikel"] ) ){
-		
+
 		$dblink = mysql_connect( $dbhost, $dbuser, $dbpass ) or
 			die( 'Keine Verbindung möglich: ' . mysql_error() );
 		mysql_select_db( $dbname );
@@ -45,7 +45,7 @@ $refresh=15;
 		foreach( $_SESSION["artikel"] as $key => $val ) {
 			if (!empty($val))
 			{
-				$sql = 'SELECT bu.id AS id, barcode, netto, gewicht, titel, au.name AS autor FROM 
+				$sql = 'SELECT bu.id AS id, barcode, netto, gewicht, titel, au.name AS autor FROM
 						buecher AS bu JOIN autor AS au ON au.id = bu.autorid
 						JOIN verlag AS ve ON ve.id = bu.verlagsid
 						where (
@@ -58,12 +58,12 @@ $refresh=15;
 				}
 
 				$row = mysql_fetch_array( $result );
-		
+
 				$summe += $row["netto"] * $val;
-				$OrderPosition[] = array("ISBN" => $row["barcode"], "Quantity" => $val);			
+				$OrderPosition[] = array("ISBN" => $row["barcode"], "Quantity" => $val);
 			}
 		}
-		
+
 	}
 
 	try
@@ -80,19 +80,20 @@ $refresh=15;
 			));
 
 		echo "DoOrder(...) called successfully - TrackingID: ".$result->DoOrderResult;
-		
+
 		// Debug ouptput
 		soapDebug($client);
 	}
 	catch(SoapFault $exception)
 	{
 		soapDebug($client);
-		
+
 		echo "<h3>Error</h3>";
 		throw $exception;
 	}
-	
-	
+
+	unset($_SESSION['artikel']);
+
 	//////////////////////////////////////////////////////////////
 	// Functions
 	function prettyXml($xmlText)
@@ -101,7 +102,7 @@ $refresh=15;
 		$dom->preserveWhiteSpace = false;
 		$dom->formatOutput = true;
 		$dom->loadXML($xmlText);
-		
+
 		return $dom->saveXML();
 	}
 
@@ -120,6 +121,7 @@ $refresh=15;
 		echo '<code>' . nl2br(htmlspecialchars($responseHeaders, true)) . '</code>' . "<br/>\n";
 		echo highlight_string($response, true) . "<br/>\n";
 	}
+	
 ?>
 	</body>
 </html>
